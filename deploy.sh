8f7b84c9ef83d2ce2b67d7986e87ba0c44177e46
Mohitsharma97 <mohitsharma150497@gmail.com>
2021-09-09 19:15:51 +0530

    travis code changed

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t mohit1504/multi-client:latest -t mohit1504/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t mohit1504/multi-server:latest -t mohit1504/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t mohit1504/multi-worker:latest -t mohit1504/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push mohit1504/multi-client:latest
 docker push mohit1504/multi-server:latest
 docker push mohit1504/multi-worker:latest
@@ -13,4 +14,3 @@ kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=mohit1504/multi-server:$SHA
 kubectl set image deployments/client-deployment client=mohit1504/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=mohit1504/multi-worker:$SHA
-
